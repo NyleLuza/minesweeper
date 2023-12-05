5d7f8370f605b942ff548969732008f55528f993
NyleLuza <jluza21@gmail.com>
2023-12-05 15:06:31 -0800

    uncover needs work

--- minesweeper.c
+++ minesweeper.c
@@ -34,10 +34,11 @@ int mines;
 void display_cell(cell* c) {
     //printf("%4d", c->position);
     //printf("%3s", "/");
-    if (c->mined == 1) printf("%2s", "*");
-    else if(c->flagged==1) printf("%2s", "P");
+
+    if (c->flagged==1) printf("%2c", 'P');
     else if(c->covered==1) printf("%2s", "/");
-    else if (c->adjcount > 0) printf("%2c", c->adjcount);
+    else if(c->mined == 1) printf("%2s", "*");
+    else if (c->adjcount > 0) printf("%2i", c->adjcount);
     else printf("%2s",".");
 }
 
@@ -50,6 +51,14 @@ void init_cell(cell *c, int p) {
 }
 int get_random(int range){
     return ((int)floor((float)range*rand()/RAND_MAX))%range;
+}
+
+void coverall(){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            board[i][j].covered = 1;
+        }
+    }
 }
 
 void command_new() {
@@ -65,8 +74,8 @@ void command_new() {
 		}
 	}
     for(int p=0;p<mines-1;p++){
-        int r = get_random(rows-1);
-        int c = get_random(cols-1);
+        int r = get_random(rows);
+        int c = get_random(cols);
         while(board[r][c].mined==1){
             r = get_random(rows);
             c = get_random(cols);
@@ -115,13 +124,43 @@ void command_flag(int r, int c){
 
 }
 
+void command_unflag(int r, int c){
+    if(board[r][c].flagged==1){
+        board[r][c].flagged = 0;
+    }
+    else{
+        printf("cell already unflagged\n");
+    }
+}
+
+
 void uncover_recursive(int r, int c){
-    board[r][c].covered == 0;
+    board[r][c].covered = 0;
     if(board[r][c].adjcount==0){
-        for(int n=0;<n<board[r][c].adjcount;n++){}
+        int neighborhoodcount = 8;
+        int rowneighbors[] = {-1,-1,+0,+1,+1,+1,+0,-1};
+        int colneighbors[] = {+0,+1,+1,+1,+0,-1,-1,-1};
+                for(int d=0;d<7;d++){
+                    int r2=r+rowneighbors[d];
+                    int c2=c+colneighbors[d];
+                        if(0 <= r2 && r2 < rows && 0 <= c2 && c2 < cols) {
+                            if (board[r2][c2].covered==1) {
+                                if(board[r2][c2].mined == 1){
+                                    board[r2][c2].covered=1;
+                                }
+                                else {
+                                    uncover_recursive(r2, c2);
+                                }
+                            }
+                        }
+                }
+
+
 
     }
 }
+
+
 
 int processcommand(char tokens[][MAXTOKENLENGTH], int tokencount) {
 	if (strcmp(tokens[0], "new") == 0) {
@@ -133,9 +172,25 @@ int processcommand(char tokens[][MAXTOKENLENGTH], int tokencount) {
 	else if (strcmp(tokens[0], "show") == 0)
 		command_show();
 
-    else if(strcmp(tokens[0], "flag")== 0)
-        command_flag(rows,cols);
+    else if (strcmp(tokens[0], "coverall") == 0)
+        coverall();
 
+    else if(strcmp(tokens[0], "flag")== 0) {
+        int first = atoi(tokens[1]);
+        int second = atoi(tokens[2]);
+        command_flag(first, second);
+    }
+    else if(strcmp(tokens[0], "unflag")== 0) {
+        int first = atoi(tokens[1]);
+        int second = atoi(tokens[2]);
+        command_unflag(first, second);
+    }
+
+    else if (strcmp(tokens[0], "uncover") == 0) {
+        int first = atoi(tokens[1]);
+        int second = atoi(tokens[2]);
+        uncover_recursive(first, second);
+    }
 	else if (strcmp(tokens[0], "quit") == 0)
 		return 0;
 
